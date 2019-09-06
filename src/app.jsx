aecd971da0910c67d453712deeda3f1f2d108838
Luke Liu <cacocacoon@gmail.com>
2019-09-06 20:11:47 +0800

    init project

---
+++ src/app.jsx
@@ -0,0 +1,4 @@
+import React from 'react'
+import './style.scss'
+
+console.log(<div>Hello World!</div>)
---
+++ webpack.config.js
@@ -0,0 +1,50 @@
+const path = require('path')
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin")
+const APP_DIR = path.resolve(__dirname, 'src')
+const BUILD_DIR = path.resolve(__dirname, 'docs')
+
+const config = {
+    entry: APP_DIR + '/app.jsx',
+    output: {
+        path: BUILD_DIR,
+        filename: 'app.js'
+    },
+    module: {
+        rules: [{
+            test: /\.jsx?$/,
+            exclude: /node_modules/,
+            use: ['babel-loader', 'eslint-loader'],
+        }, {
+            test: /\.scss$/,
+            use: [MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader', 'sass-loader']
+        }]
+    },
+    resolve: {
+		extensions: ['.js', '.jsx']
+	},
+    devtool: 'source-map',
+    devServer: {
+		inline: true,
+		contentBase: BUILD_DIR,
+		port: 8008
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Daily Drinks',
+            meta: {
+                description: 'Order drinks',
+                viewport: 'width=device-width, initial-scale=1, maximum-scale=1',
+            },
+            template: './index.html',
+            hash: true,
+            cache: false,
+        }),
+        new MiniCssExtractPlugin({
+            filename: 'style.[hash].css'
+        })
+    ]
+}
+
+module.exports = config
+
